3f9ff8974cc3bc4eb6a2f936cd5e17ca809d01e2
shun2218-dev <kohaku1015game@gmail.com>
2022-10-07 06:39:30 +0900

    header

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref, Ref } from "@vue/reactivity";
+import Header from "./components/Header.vue";
+
+const name = ref("");
+const email = ref("");
+const activate = ref(0);
+const menus = ["Form", "List"];
+
+const onSubmit = () => {
+  console.log(name.value, email.value);
+};
+</script>
+
+<template>
+  <Header :menus="menus" :activate="activate" />
+  <form action="" @submit.prevent="onSubmit" class="form">
+    <input type="text" v-model="name" />
+    <input type="email" v-model="email" />
+    <button>submit</button>
+  </form>
+</template>
+
+<style scoped lang="scss">
+.form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { computed, Ref, ref } from "vue";
+
+export type Props = {
+  menus: string[];
+  activate: Ref<number>;
+};
+const props = defineProps<Props>();
+const isActive = (index: number, activate: number) => (activate = index);
+</script>
+
+<template>
+  <header class="header">
+    <nav>
+      <ul class="menu">
+        <li
+          v-for="(menu, index) in menus"
+          :key="menu"
+          @click="isActive(index, activate.value)"
+          class="menuItem"
+          :class="{
+            activate: activate.value === index,
+          }"
+        >
+          {{ menu }}
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 3%;
+  box-shadow: 2px 2px 2px 1px #323232;
+}
+.menu {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  gap: 10%;
+}
+
+.menuItem {
+  cursor: pointer;
+}
+.activate {
+  text-decoration-line: underline;
+  font-weight: bold;
+  font-size: 1.2rem;
+}
+</style>
